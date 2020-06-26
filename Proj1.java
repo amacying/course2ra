ca1300bff8a3454198b37476f4a0913d41eaf569
macy huang <macyhuang@macys-mbp.attlocal.net>
2020-06-26 12:20:04 -0500

    Add class

---
+++ Proj1.java
@@ -0,0 +1,8 @@
+public class Proj1 {
+    public static void main(String[] args) {
+        int num = 80;
+        System.out.println("Hi I'm Macy." + num); //this ia a comment
+        System.out.printf("Num is %d   %s", num, "No");
+        /*yyyy*/
+    }
+}
